6bba21bf514cba2d96304fdf3a62b10791986c6f
darklord9201 <darklord9201@live.com>
2014-10-08 22:31:36 +0545

    Added Styling and Navigation Bar

---
+++ application/views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap.js"></script>
+</head>
+
+<body>
+	<div class="container">
+		
---
+++ application/views/navigation.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="container">			
+				<center><a class="navbar-brand" href="#">Payroll System</a></center>
+			</div>
+</nav>
+<div class="navbar-custom col-sm-2">
+	<ul class="nav nav-stacked">		
+		<li class="info"><img src="<?php echo base_url();?>images/userIcon.png" height="50px"> Employee Name</li>
+		<li><a href="">abcd</a></li>
+		<li><a href="">abcd</a></li>
+		<li><a href="">abcd</a></li>
+		<li><a href="">abcd</a></li>		
+	</ul>	
+	
+</div>
+
+
